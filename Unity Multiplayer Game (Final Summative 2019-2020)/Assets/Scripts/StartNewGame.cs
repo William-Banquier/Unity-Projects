632930b7d775f6a81c7e97e3bb89129d1311c690
William Banquier <william.banquier@ucc.on.ca>
2020-06-04 09:37:52 -0400

    Updates infections, and gameover

---
+++ Unity Multiplayer Game (Final Summative 2019-2020)/Assets/Scripts/StartNewGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartNewGame : MonoBehaviour
+{
+    public bool gameStarted = false;
+
+    void Update()
+    {
+        if(gameStarted = false){
+            if(GameObject.FindGameObjectsWithTag("Player").Length > 1){
+                GameObject.FindGameObjectsWithTag("Player")[Random.Range(0, GameObject.FindGameObjectsWithTag("Player").Length)].tag = "I";
+                
+            }
+        }
+    }
+}
--- Unity Multiplayer Game (Final Summative 2019-2020)/Assets/Scripts/infectedCount.cs
+++ Unity Multiplayer Game (Final Summative 2019-2020)/Assets/Scripts/infectedCount.cs
@@ -6,9 +6,28 @@ using UnityEngine.UI;
 public class infectedCount : MonoBehaviour
 {
     public Text iCount;
+    public Text gameOver;
+
+    public bool gameStarted = false;
+
+    public bool noMorePlayers = false;
 
     void Update()
     {
         iCount.text = "Infected: "+(GameObject.FindGameObjectsWithTag("I").Length);
-    }
+
+        if(GameObject.FindGameObjectsWithTag("Player").Length == 0){
+            noMorePlayers = true;
+            //Debug.Log("No more players");
+        }else{
+            noMorePlayers = false;
+        }
+
+        if (noMorePlayers == true){
+            gameOver.text = "Gameover: True";
+        }else{
+            gameOver.text= "Gameover: False";
+        }
+        
+    }   
 }
